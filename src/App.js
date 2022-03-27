fffe6773f41d70c77dae9dfd8847683118bdf1d5
nitin <nitinspatil@gmail.com>
2022-03-27 19:42:01 +0530

    added

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import MainComponent from "./components/MainComponent";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./components/assets/stylesheet/styles.css";
+function App() {
+  return <MainComponent />;
+}
+
+export default App;
